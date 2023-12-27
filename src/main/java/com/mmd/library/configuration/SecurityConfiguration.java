d1ddc52c680266a3ad711da717523dc75367b87e
Mohammed Othman <mahamadsaber18@gmail.com>
2023-12-27 16:03:54 +0300

    Added basic security class configuration

---
+++ src/main/java/com/mmd/library/configuration/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package com.mmd.library.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(requests -> requests.anyRequest().permitAll())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
