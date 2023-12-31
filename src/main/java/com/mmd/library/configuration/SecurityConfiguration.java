0f6d6243b4440e5210e25819f868412a5eb91cc6
Mohammed Othman <mahamadsaber18@gmail.com>
2023-12-31 23:17:35 +0300

    Adding authenticationFailure/SuccessHandler
    Added both and used them to add a header to the response to the client of Success or Failure accordingly to let the client know if auth was successful or not, I also needed to expose both as Headers in corsConfigurationSource. Also needed to make sure formLogin in securityFilterChain uses both as successHandler and failureHandler

--- src/main/java/com/mmd/library/configuration/SecurityConfiguration.java
+++ src/main/java/com/mmd/library/configuration/SecurityConfiguration.java
@@ -1,42 +1,84 @@
 package com.mmd.library.configuration;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
 @Configuration
 public class SecurityConfiguration {
 
-//    @Bean
-//    CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // Allow all origins
-//        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","PATCH","DELETE","OPTIONS")); // Allow all methods
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
-
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests(requests -> requests.anyRequest().permitAll())
-                .formLogin(Customizer.withDefaults())
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(requests -> requests.requestMatchers("/login").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(fr -> fr.successHandler(authenticationSuccessHandler())
+                                    .failureHandler(authenticationFailureHandler()))
                 .httpBasic(Customizer.withDefaults())
-                .csrf(AbstractHttpConfigurer::disable);
+                .cors(cors-> cors.configurationSource(corsConfigurationSource()));
 
 //        http.csrf(customizer  -> customizer.ignoringRequestMatchers("api//books/reviews"));
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.applyPermitDefaultValues();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // Allow all origins
+        configuration.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS")); // Allow all methods
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setExposedHeaders(List.of("Authorization", "Success", "Failure"));//Exposing header from backend to frontend
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    AuthenticationSuccessHandler authenticationSuccessHandler(){
+        return new LibraryAuthenticationSuccessHandler();
+    }
+
+    @Bean
+    AuthenticationFailureHandler authenticationFailureHandler() {
+        return new LibraryAuthenticationFailureHandler();
+    }
+
+    static class LibraryAuthenticationSuccessHandler implements AuthenticationSuccessHandler{
+        @Override
+        public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+            response.addHeader("Success", "Success authentication");
+        }
+    }
+
+    static class LibraryAuthenticationFailureHandler implements AuthenticationFailureHandler{
+        @Override
+        public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+            response.addHeader("Failure", exception.getMessage());
+        }
     }
 
     @Bean
